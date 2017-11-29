d0f97b6d3ef78f97126658badae1764d7d6d2cba
hannah <hannah.munoz92@gmail.com>
2017-11-29 02:43:33 -1200

    more work done!

--- app/app.js
+++ app/app.js
@@ -3,31 +3,77 @@ var phonecatApp = angular.module('phonecatApp', []);
 var FilePath = "/Patterns";
 var patternLimit = 3;
 
-var patterns = [];
 
 phonecatApp.controller('ServiceController', function ServiceController($scope, $http, $q) {
+  $scope.patterns = [];
+  $scope.limit = 2;
+  $scope.checked = 0;
+
   var init = function(){
-      $q (function(success, failure){
-          for (var i = 0; i < patternLimit; i++ ){
-            FileRead(i).then (function(response){
-              console.log (response);
-              if (response == patternLimit-1){
-                success(response);
-              }
-            })
-          }  
-      }).then (function success(response){ 
-      console.log (patterns);
-      console.log (patterns.length);
-      console.log (patterns[1]);
+    $q (function(success, failure){
+      for (var i = 0; i < patternLimit; i++ ){
+        FileRead(i).then (function(response){
+          if (response == patternLimit-1){
+            success($scope.response);
+          }
+        })
+      }
+    }).then (function success(response){
+      console.log ($scope.patterns);
+      console.log ($scope.patterns[0]);
     })
 
   }
 
+  $scope.checkChanged = function(i){
+    if($scope.patterns[i].selected) $scope.checked++;
+    else $scope.checked--;
+  }
+
+  $scope.next = function(){
+    data=[];
+    for (var i = 0; i < patternLimit; i++ ){
+        if ($scope.patterns[i].selected){
+          data.push ("1");
+        }
+        else{
+          data.push ("0");
+        }
+    }
+
+    console.log (data);
+
+  //  $http.post (FilePath+ "/final.txt", data);
+
+  var blob = new Blob([data], {type: 'text/plain'}),
+           e    = document.createEvent('MouseEvents'),
+           a    = document.createElement('a')
+// FOR IE:
+
+ if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+     window.navigator.msSaveOrOpenBlob(blob, FilePath+ "/final.txt");
+ }
+ else{
+     var e = document.createEvent('MouseEvents'),
+         a = document.createElement('a');
+
+     a.download = FilePath+ "/final.txt";
+     a.href = window.URL.createObjectURL(blob);
+     a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+     e.initEvent('click', true, false, window,
+         0, 0, 0, 0, 0, false, false, false, false, 0, null);
+     a.dispatchEvent(e);
+ }
+
+  }
+
+  $scope.finished = function(){
+  }
+
    function FileRead(iter){
     return $http.get(FilePath+ '/'+iter+'.txt').then(function(data) {
-           patterns.push (data.data);
-           return iter;
+      $scope.patterns.push ({data: data.data, selected: false});
+      return iter;
     });
   }
 
